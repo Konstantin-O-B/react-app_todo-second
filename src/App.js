6c34b6d0355a3899706abf59b895ae5955377e32
Konstantin <nickel7772008@yandex.ru>
2023-06-29 19:46:15 +0700

    1

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ function App() {
   };
 
   const clearCompleted = () => {
-    const clearTasks = [...arrayTasks].filter((item) => item.status);
+    const clearTasks = [...arrayTasks].filter((item) => item.status === 'completed');
     setArrayTasks(clearTasks);
   };
 
